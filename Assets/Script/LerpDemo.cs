49c57f5e66a51b8725cc16eb30a09c5df62b303a
Anita Hosseini <hosseani@shernet.sheridancollege.ca>
2025-01-16 14:48:28 -0500

    Added a start and end, also made the circle move from start to end.

---
+++ Assets/Script/LerpDemo.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpDemo : MonoBehaviour
+{
+
+    [Range(0, 1)]
+    public float t;
+    public AnimationCurve curve;
+    public Transform start;
+    public Transform end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+
+        if (t > 1)
+        {
+            t = 0;
+        }
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+
+    }
+}
