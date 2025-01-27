41cf49512a2efe6d37ce7f9b1d943cce690cb6d4
Anita Hosseini <hosseani@shernet.sheridancollege.ca>
2025-01-27 13:33:14 -0500

    Fixed the twinkle speed of the stars and made the grassy sway by following the mouse position

--- Assets/Script/GrassSwaying.cs
+++ Assets/Script/GrassSwaying.cs
@@ -11,8 +11,13 @@ public class GrassSwaying : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //Makes the triangles follow the position of the mouse at an offset to create a swaying effect
+        //I put this script on an empty game object to create a makeshift pivot and set that as the parent of each grass blade
     {
-        
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = 0;
+        Vector2 direction = mouse - transform.position;
+
+        transform.up = direction; 
     }
 }
--- Assets/Script/StarsTwinkling.cs
+++ Assets/Script/StarsTwinkling.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class StarsTwinkling : MonoBehaviour
 {
-    Vector2 starScale;
-    public float speed = 0.1f;
-    public AnimationCurve curve;
+    Vector2 starScale; //setting the scale of the starts to a costum scale
+    public float speed = 0.1f; //Declaring a speed variable so I can change the speed using a speed handle
+    public AnimationCurve curve;//using animation curve to make the stars pop in and out, using different graphs to create a more natural twinkle effect
     [Range(0, 1)]
     public float t;
     // Start is called before the first frame update
@@ -18,12 +18,14 @@ public class StarsTwinkling : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
+
+        t += Time.deltaTime + speed;
 
         if (t > 1)
         {
             t = 0;
         }
         transform.localScale = starScale * curve.Evaluate(t);
+
     }
 }
