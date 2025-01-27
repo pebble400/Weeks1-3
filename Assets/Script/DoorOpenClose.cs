304374b908ea0c41145bc8f944a776fbb75aec2e
DESKTOP-KA9TBRQ\Owner <anitahosseini37@gmail.com>
2025-01-27 17:36:47 -0500

    Added the fidget spinner and made it so that it spins if the space bar is held down.

--- Assets/Script/DoorOpenClose.cs
+++ Assets/Script/DoorOpenClose.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorOpenClose : MonoBehaviour
-{
+{//using both lerp and curve to create a door sliding animation for when the door is clicked
     [Range(0, 1)]
     public float t;
     public AnimationCurve curve;
     public Transform start;
     public Transform end;
-    bool isOpen = false;
+    bool isOpen = false; 
     
     // Start is called before the first frame update
     void Start()
@@ -23,16 +23,16 @@ public class DoorOpenClose : MonoBehaviour
         Vector2 pos = transform.position;
         pos.y = curve.Evaluate(t);
 
-        if (Input.GetMouseButtonDown(0) || t > 0)
+        if (Input.GetMouseButtonDown(0) || t > 0)//if the door is clicked and if the variable t (time) is greater than zero, t will go up by one each frame and change the position of the door
         {
             isOpen = true;
             t += Time.deltaTime;
-            transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+            transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));//setting the start position of the door
         }
         else if (isOpen == true)
         {
             t -= Time.deltaTime;
-            transform.position = Vector2.Lerp(end.position, start.position, curve.Evaluate(t));
+            transform.position = Vector2.Lerp(end.position, start.position, curve.Evaluate(t));//setting the end position of the door
 
         }
     }
--- Assets/Script/FidgetSpinner.cs
+++ Assets/Script/FidgetSpinner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FidgetSpinner : MonoBehaviour
 {
+    float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,13 @@ public class FidgetSpinner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 rot = transform.eulerAngles;//assigning the position 
+        rot.z += speed;
+
+
+        if (Input.GetKey(KeyCode.Space))//rotates the fidget spinner only if the space bar is pressed
+        {
+            transform.eulerAngles = rot;
+        }
     }
 }
